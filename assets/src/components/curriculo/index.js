710d6376f5050ec0a5ba062265eddbbb427e1e5a
Mauricio Festa <mauricio.festa@icloud.com>
2021-05-05 06:09:58 -0300

    Curriculos modulo arquivos novos

---
+++ assets/src/components/curriculo/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default class Curriculo extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    //this.state = {msg: true,variant: "warning", text: "null"};
+   // this.validaLogin = this.validaLogin = this.validaLogin.bind(this);
+
+  }
+
+    render() {
+      return (
+
+        <div className='container mt-4 main'>
+
+          <h1>Heloo</h1>
+
+        </div>
+
+      );
+    }
+}
