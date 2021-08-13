19ad575c36f5cca60c1b0ed1b35b450085ba5640
Mauricio Festa <mauricio@localhost.localdomain>
2021-08-13 13:09:08 -0300

    permission files

---
+++ cqlsh.sh
@@ -0,0 +1,1 @@
+cd /home/mauricio/Documents/cassandra/apache-cassandra-3.11.11 && bin/cqlsh
---
+++ start_cassandra.sh
@@ -0,0 +1,1 @@
+cd /home/mauricio/Documents/cassandra/apache-cassandra-3.11.11 && sudo bin/cassandra -f -R
