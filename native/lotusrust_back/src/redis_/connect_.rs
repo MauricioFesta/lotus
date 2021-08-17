1a8536d1a19e42465bc1aaad05e9dacd0ab91fc4
root <root@localhost.localdomain>
2021-08-17 12:53:21 -0300

    okok

--- native/lotusrust_back/src/redis_/connect_.rs
+++ native/lotusrust_back/src/redis_/connect_.rs
@@ -10,11 +10,3 @@ pub fn main() -> redis::Connection {
   
 }
 
-// pub fn main() -> redis::Connection {
-
-//     let client = redis::Client::open("redis://127.0.0.1/").unwrap();
-//     let mut con = client.get_connection().unwrap();
-
-//     con
-  
-// }
