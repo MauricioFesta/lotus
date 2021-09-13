5157999e0fbb0ecc16b88628041122e579ac5941
root <root@localhost.localdomain>
2021-09-13 05:51:01 -0400

    config new addres cassandra

--- native/lotusrust_back/src/cassandra/connect.rs
+++ native/lotusrust_back/src/cassandra/connect.rs
@@ -20,10 +20,10 @@ type CurrentSession = Session<RoundRobin<TcpConnectionPool<StaticPasswordAuthent
 
 pub fn conn() -> CurrentSession {
 
-    let user = "user";
-    let password = "password";
+    let user = "lotus_root";
+    let password = "nuOTbtK$B8G%#0I$w7@";
     let auth = StaticPasswordAuthenticator::new(&user, &password);
-    let node = NodeTcpConfigBuilder::new("localhost:9042", auth).build();
+    let node = NodeTcpConfigBuilder::new("137.184.9.0:9042", auth).build();
     let cluster_config = ClusterTcpConfig(vec![node]);
     let no_compression: CurrentSession = new_session(&cluster_config, RoundRobin::new()).expect("session should be created");
 
