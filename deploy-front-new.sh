89761d2064ab6e509839a182140f72ae061d3780
root <root@localhost.localdomain>
2022-02-13 19:53:21 -0500

    new deploy

--- deploy-front-new.sh
+++ deploy-front-new.sh
@@ -4,4 +4,4 @@ cd assets
 
 cp -r build/* /local_deploy_front/build
 
-rsync -avh -e "ssh -p 62451 -i $HOME/.ssh/oracle-cloud.key" /local_deploy_front/build -r root@150.136.242.162:/usr/local/bin/proxy_node/app
+rsync -avh -e "ssh -p 62451 -i $HOME/.ssh/oracle-cloud.key" /local_deploy_front/build -r root@150.136.242.162:/usr/local/bin/lotus_front/app
