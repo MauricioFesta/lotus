e1e940aa6a6d98c2cda51a63fe56b1af267ce3c2
MauricioFesta <52216532+MauricioFesta@users.noreply.github.com>
2022-03-11 15:02:23 -0300

    Update cassandra.ex
    error sintax

--- lib/lotus/cassandra.ex
+++ lib/lotus/cassandra.ex
@@ -5,15 +5,18 @@ defmodule Lotus.Cassandra do
 		{:ok, ips} = :inet.getif()
 
 		{vl1,vl2,vl3,vl4} = ips |> Enum.map(fn x -> x |> elem(0)  end) |> Enum.at(1)
+		
+		
+	"10.0.11.21:9042"
 
-        cond do
+        #cond do
 		
 	    "10.0.11.21:9042"
             #{vl1,vl2,vl3,vl4} == {137,184,9,0} -> "#{vl1}.#{vl2}.#{vl3}.#{vl4}:9042"
 
             #true -> "127.0.0.1:9042"
 
-        end
+        #end
 
 		
 
