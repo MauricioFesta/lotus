f9b74bd521ddf2210163a49c84ac6b3287485e68
Mauricio Festa <mauricio.festa@icloud.com>
2022-08-10 20:54:35 -0300

    validation ip

--- lib/lotus/cassandra.ex
+++ lib/lotus/cassandra.ex
@@ -2,24 +2,14 @@ defmodule Lotus.Cassandra do
 
 	def get_ip do	
 
-		{:ok, ips} = :inet.getif()
-
-      ip = ips |> Enum.map(fn x -> x |> elem(0)  end) |> Enum.at(1)
+    {:ok, hostname} = :inet.gethostname
 		
 
         cond do
 		
-	        ip == {172, 20, 0, 1} -> "127.0.0.1:9042"
-
-			ip == {192, 168, 0, 108}-> "127.0.0.1:9042"
-
-			ip == {192, 168,100,102} -> "127.0.0.1:9042"
-
-			ip == {192,168,1,15} -> "127.0.0.1:9042"
-
-	        ip == {172, 19, 0, 1}  -> "127.0.0.1:9042"
-			
-            true -> "10.0.11.148:9042"
+	      hostname == 'mauricio-aspirea51453'  -> "127.0.0.1:9042"
+          
+          true -> "10.0.11.148:9042"
 
         end 
 
